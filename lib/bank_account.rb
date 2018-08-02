2b32454806339f714aad42f03d67d6b1a862a063
Koy Saeteurn <kosatun@aol.com>
2018-08-02 21:47:46 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+  attr_reader :name 
+  attr_accessor :balance, :status
+  
+  @@deposit=[]
+  
+  def initialize(name)
+    @name = name 
+    @balance = balance
+    @status = status 
+  end  
+  
+  def self.all 
+    @@deposit
+  end   
+  
+  def balance 
+    @balance = 1000
+    @deposit << balance
+  end 
+  
+  def status 
+    @status = "open"
+  end 
+  
+  def deposit(money)
+    @@deposit << money
+  end   
 
 end
