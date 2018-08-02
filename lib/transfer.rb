07883f1f39982a8a0d8783212e2259a8bd54e972
Koy Saeteurn <kosatun@aol.com>
2018-08-02 22:28:37 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,11 +10,7 @@ class Transfer
   end   
     
   def valid? 
-    if @status = "open"
-      true 
-    else 
-      false 
-    end  
+    sender.valid? && receiver.valid?
   end   
   
   def execute_transaction
